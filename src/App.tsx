c16a457c5179a4d8301f7eacff9a639f58f88ef9
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-08-11 20:22:54 -0300

    add task solution after code review

--- src/App.tsx
+++ src/App.tsx
@@ -15,64 +15,62 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  Default = 'Default',
+  Alphabetical = 'Alphabetical',
+  ByLength = 'ByLength',
+  Reverse = 'Reverse',
+}
+
 export const App: React.FC = () => {
-  const [inSort, setInSort] = React.useState(goodsFromServer);
+  const [sortType, setSortType] = React.useState<SortType>(SortType.Default);
 
-  const organizeAlphabetical = (list: string[]): string[] => {
-    return [...list].sort((a, b) => a.localeCompare(b));
-  };
-
-  const organizeByLength = (list: string[]): string[] => {
-    return [...list].sort((a, b) => a.length - b.length);
-  };
-
-  const organizeReverse = (list: string[]): string[] => {
-    return [...list].reverse();
-  };
+  const sortedGoods = React.useMemo(() => {
+    switch (sortType) {
+      case SortType.Alphabetical:
+        return [...goodsFromServer].sort((a, b) => a.localeCompare(b));
+      case SortType.ByLength:
+        return [...goodsFromServer].sort((a, b) => a.length - b.length);
+      case SortType.Reverse:
+        return [...goodsFromServer].slice().reverse();
+      default:
+        return goodsFromServer;
+    }
+  }, [sortType]);
 
   return (
-    <div className="section content">
-      <div className="buttons">
-        <button
-          type="button"
-          className="button is-info is-light"
-          onClick={() => setInSort(organizeAlphabetical(inSort))}
-        >
-          Sort alphabetically
-        </button>
+    <div className="buttons">
+      <button
+        type="button"
+        className={`button is-info ${sortType === SortType.Alphabetical ? '' : 'is-light'}`}
+        onClick={() => setSortType(SortType.Alphabetical)}
+      >
+        Sort alphabetically
+      </button>
 
-        <button
-          type="button"
-          className="button is-success is-light"
-          onClick={() => setInSort(organizeByLength(inSort))}
-        >
-          Sort by length
-        </button>
+      <button
+        type="button"
+        className={`button is-success ${sortType === SortType.ByLength ? '' : 'is-light'}`}
+        onClick={() => setSortType(SortType.ByLength)}
+      >
+        Sort by length
+      </button>
 
-        <button
-          type="button"
-          className="button is-warning is-light"
-          onClick={() => setInSort(organizeReverse(inSort))}
-        >
-          Reverse
-        </button>
+      <button
+        type="button"
+        className={`button is-warning ${sortType === SortType.Reverse ? '' : 'is-light'}`}
+        onClick={() => setSortType(SortType.Reverse)}
+      >
+        Reverse
+      </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-          onClick={() => setInSort(goodsFromServer)}
-        >
-          Reset
-        </button>
-      </div>
-
-      <ul>
-        {inSort.map(iten => (
-          <li key={iten} data-cy="Good">
-            {iten}
-          </li>
-        ))}
-      </ul>
+      <button
+        type="button"
+        className={`button is-danger ${sortType === SortType.Default ? '' : 'is-light'}`}
+        onClick={() => setSortType(SortType.Default)}
+      >
+        Reset
+      </button>
     </div>
   );
 };
